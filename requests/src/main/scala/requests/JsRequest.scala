b27f25b7b74c1f3a483122302cb04fd745c8b1e7
johannes karoff <johannes@karoff.net>
2021-04-05 01:28:01 +0200

    add headers to api

--- requests/src/main/scala/requests/JsRequest.scala
+++ requests/src/main/scala/requests/JsRequest.scala
@@ -5,13 +5,13 @@ import scala.concurrent.{Future, ExecutionContext}
 import scala.scalajs.js
 import scala.scalajs.js.JSConverters._
 
-import org.scalajs.dom.experimental.{BodyInit, Fetch, RequestInit, HttpMethod, ReadableStreamReader, Response}
+import org.scalajs.dom.experimental.{BodyInit, Fetch, RequestInit, HttpMethod, ReadableStreamReader, Response, Headers}
 
 trait JsRequest[T] {
   def request(method: HttpMethod, url: String, payload: Option[T])(implicit ec: ExecutionContext): Future[T]
 
-  final def fetchData(method: HttpMethod, url: String, payload: js.UndefOr[BodyInit]): Future[Response] =
-    Fetch.fetch(url, js.Dynamic.literal("method" -> method, "body" -> payload.asInstanceOf[js.Any]).asInstanceOf[RequestInit]).toFuture
+  final def fetchData(method: HttpMethod, url: String, payload: js.UndefOr[BodyInit] = js.undefined, headers: js.UndefOr[Headers] = js.undefined): Future[Response] =
+    Fetch.fetch(url, js.Dynamic.literal("method" -> method, "body" -> payload.asInstanceOf[js.Any], "headers" -> headers.asInstanceOf[js.Any]).asInstanceOf[RequestInit]).toFuture
 
   final def get(url: String)(implicit ec: ExecutionContext): Future[T] = request(HttpMethod.GET, url, None)
   final def post(url: String, payload: T)(implicit ec: ExecutionContext): Future[T] = request(HttpMethod.GET, url, Some(payload))
