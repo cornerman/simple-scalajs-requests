0201b2ada857cf92684f14d8f70dd04ca4e2367b
johannes karoff <johannes@karoff.net>
2021-10-29 23:43:04 +0200

    add headers

--- requests/src/main/scala/requests/JsRequest.scala
+++ requests/src/main/scala/requests/JsRequest.scala
@@ -8,21 +8,21 @@ import scala.scalajs.js.JSConverters._
 import org.scalajs.dom.experimental.{BodyInit, Fetch, RequestInit, HttpMethod, ReadableStreamReader, Response, Headers}
 
 trait JsRequest[T] {
-  def request(method: HttpMethod, url: String, payload: Option[T])(implicit ec: ExecutionContext): Future[T]
+  def request(method: HttpMethod, url: String, payload: Option[T], header: Map[String, String] = Map.empty)(implicit ec: ExecutionContext): Future[T]
 
-  final def fetchData(method: HttpMethod, url: String, payload: js.UndefOr[BodyInit] = js.undefined, headers: js.UndefOr[Headers] = js.undefined): Future[Response] =
-    Fetch.fetch(url, js.Dynamic.literal("method" -> method, "body" -> payload.asInstanceOf[js.Any], "headers" -> headers.asInstanceOf[js.Any]).asInstanceOf[RequestInit]).toFuture
+  final def fetchData(method: HttpMethod, url: String, payload: js.UndefOr[BodyInit] = js.undefined, headers: Map[String, String] = Map.empty): Future[Response] =
+    Fetch.fetch(url, js.Dynamic.literal("method" -> method, "body" -> payload.asInstanceOf[js.Any], "headers" -> new Headers(headers.map { case (k, v) => js.Array(k,v) }.toJSArray)).asInstanceOf[RequestInit]).toFuture
 
-  final def get(url: String)(implicit ec: ExecutionContext): Future[T] = request(HttpMethod.GET, url, None)
-  final def post(url: String, payload: T)(implicit ec: ExecutionContext): Future[T] = request(HttpMethod.GET, url, Some(payload))
+  final def get(url: String, header: Map[String, String] = Map.empty)(implicit ec: ExecutionContext): Future[T] = request(HttpMethod.GET, url, None, header)
+  final def post(url: String, payload: T, header: Map[String, String] = Map.empty)(implicit ec: ExecutionContext): Future[T] = request(HttpMethod.GET, url, Some(payload), header)
 }
 
 object JsRequestBytes extends JsRequest[ByteBuffer] {
   import scala.scalajs.js.typedarray._
   import scala.scalajs.js.typedarray.TypedArrayBufferOps._
 
-  def request(method: HttpMethod, url: String, payload: Option[ByteBuffer])(implicit ec: ExecutionContext): Future[ByteBuffer] = for {
-      result <- fetchData(method, url, payload.map(_.typedArray().asInstanceOf[BodyInit]).orUndefined)
+  def request(method: HttpMethod, url: String, payload: Option[ByteBuffer], header: Map[String, String] = Map.empty)(implicit ec: ExecutionContext): Future[ByteBuffer] = for {
+      result <- fetchData(method, url, payload.map(_.typedArray().asInstanceOf[BodyInit]).orUndefined, header)
       buffers <- readBytes(result.body.getReader())
     } yield combineByteBuffers(buffers)
 
@@ -46,16 +46,16 @@ object JsRequestBytes extends JsRequest[ByteBuffer] {
 
 object JsRequestJson extends JsRequest[String] {
 
-  def request(method: HttpMethod, url: String, payload: Option[String])(implicit ec: ExecutionContext): Future[String] = for {
-      result <- fetchData(method, url, payload.orUndefined)
+  def request(method: HttpMethod, url: String, payload: Option[String], header: Map[String, String] = Map.empty)(implicit ec: ExecutionContext): Future[String] = for {
+      result <- fetchData(method, url, payload.orUndefined, header)
       json <- result.json().toFuture
     } yield json.asInstanceOf[String]
 }
 
 object JsRequestText extends JsRequest[String] {
 
-  def request(method: HttpMethod, url: String, payload: Option[String])(implicit ec: ExecutionContext): Future[String] = for {
-      result <- fetchData(method, url, payload.orUndefined)
+  def request(method: HttpMethod, url: String, payload: Option[String], header: Map[String, String] = Map.empty)(implicit ec: ExecutionContext): Future[String] = for {
+      result <- fetchData(method, url, payload.orUndefined, header)
       text <- result.text().toFuture
     } yield text.asInstanceOf[String]
 }
